9cb5801e04c9a716ef8849a2f55a6780177d6bf8
Phynatik <andreandrews313@gmail.com>
2023-04-29 11:52:22 -0400

    added postgresql adapter

---
+++ app/app/tests.py
@@ -0,0 +1,27 @@
+"""
+Sample Tests
+"""
+
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class ClassTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        expected = 2
+
+        res = calc.add(1, 1)
+
+        self.assertEqual(expected, res)
+
+    def test_subtract_numbers(self):
+        numOne, numTwo = 1, 2
+
+        expected = numTwo - numOne
+
+        res = calc.subtract(numTwo, numOne)
+
+        self.assertEqual(res, expected) 
